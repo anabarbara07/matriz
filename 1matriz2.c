977ab14e5ccb847869638cbda5e8de47cfed05e7
anabarbara07 <132527345+anabarbara07@users.noreply.github.com>
2023-07-05 00:47:12 -0300

    Add files via upload

---
+++ 1matriz2.c
@@ -0,0 +1,27 @@
+/*01- Criar um algoritmo que possa armazenar as alturas de dez atletas de cinco delegações que
+participarão dos jogos de verão. Imprimir a maior altura de cada delegação.*/
+
+#include<stdio.h>
+
+main(){
+
+int m[10][1],maior=0,c,l;
+
+for(l=0;l<1;l++)
+for(c=0;c<5;c++){
+
+for(l=0;l<10;l++)
+for(c=0;c<1;c++)
+{
+printf("Digite a altura: ");
+scanf("%d",&m[l][c]);
+
+if(m[l][c]>maior)
+    maior=m[l][c];
+
+}
+printf("\nO maior jogador tem altura de: %d\n\n",maior);
+
+}
+
+}
